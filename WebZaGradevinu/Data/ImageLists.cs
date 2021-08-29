c2bfd4f16687870b69f4089c78679ad92d87eeab
Dominik Jezidžić <djezidzic@tvz.hr>
2021-08-30 01:57:25 +0200

    Tabovi sredeni Moze se dodat u bazu ImageLists

---
+++ WebZaGradevinu/Data/ImageLists.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebZaGradevinu.Data
+{
+    public class ImageLists
+    {
+        public enum RoomType
+        {
+            Bazen,
+            Kupaona,
+            Ostalo
+        }
+
+        [Key]
+        public int ID { get; set; }
+        public string Path { get; set; }
+        public string ImageName { get; set; }
+        public RoomType Types { get; set; }
+
+    }
+}
---
+++ WebZaGradevinu/Data/WebAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebZaGradevinu.Data
+{
+    public class WebAppDbContext : DbContext
+    {
+
+        protected WebAppDbContext() { }
+
+        public WebAppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ImageLists> ImageListing { get; set; }
+
+    }
+}
