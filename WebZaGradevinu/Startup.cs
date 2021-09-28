48bc1149b105f243d308f1f4f40bc2fd143d1397
Dominik Jezidžić <djezidzic@tvz.hr>
2021-09-28 02:26:20 +0200

    -Added Modal support for all three gallery categories -Added loading option while it loads images

--- WebZaGradevinu/Startup.cs
+++ WebZaGradevinu/Startup.cs
@@ -33,7 +33,6 @@ namespace WebZaGradevinu
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddScoped<ImgUploadService>();
-            services.AddScoped<ModalService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
